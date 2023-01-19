acafd7bcb04c7f4332b41d6161b703115286ccc8
Ramsha Bilal <95581771+ramshabilal@users.noreply.github.com>
2023-01-19 04:15:39 +0400

    Update genify.py

--- genify.py
+++ genify.py
@@ -175,4 +175,11 @@ if st.button("Submit"):
     st.bokeh_chart(div)
 
     st.write("Go to Footprint Page if you have completed the authorization")
+    
+    st.markdown("""
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+""", unsafe_allow_html=True)
+
 
